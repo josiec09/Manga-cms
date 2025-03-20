8379fba49b2d774bb1665d1d9f1aa08d2d76a097
d3str0y3r <itzjmo@hotmail.com>
2025-03-20 16:24:34 -0500

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,25 @@
+<?php
+// database hostname - you don't usually need to change this
+define('db_host','localhost');
+// database username
+define('db_user','');
+// database password
+define('db_pass','');
+// database name
+define('db_name','');
+// database charset - change this only if utf8 is unsupported by your language
+define('db_charset','utf8');
+/* Registration */
+define('auto_login_after_register',false);
+/* Account Activation */
+// Email activation variables
+// account activation required?
+define('account_activation',true);
+// Change "Your Company Name" and "yourdomain.com" - do not remove the < and > characters
+define('mail_from','Demo <noreply@demo.com>');
+// Link to activation file
+define('activation_link','https://demo.com/activate');
+
+// Home Page manga
+define('manga_per_page','15');
+?>
